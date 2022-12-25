cb8ef1089b1ced07cb3eb26ed7279c4178f236ad
FawwazKats <fawwazkautsar1907912@upi.edu>
2022-12-25 23:05:23 +0700

    Membuat component Footer & convert file .js to .jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,3 +6,10 @@ function MyApp({ Component, pageProps }){
 }
 
 export default MyApp;
+
+/*
+    API Data : 
+    https://covid19.go.id/id/dokumentasi-api
+    https://api.covid19api.com/
+    https://documenter.getpostman.com/view/10808728/SzS8rjbc
+*/
